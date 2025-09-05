ac5c612c81e4b9abcaa801f9f524e0e90e0af9cf
2dor <poweredby2dor@gmail.com>
2025-09-05 23:01:03 +0300

    Day 1 commit

--- Day 1/task/task4.py
+++ Day 1/task/task4.py
@@ -6,8 +6,10 @@ This module contains activities referencing 100 Days of Python course.
 # Python Variables
 name = input("What is your name Sir?")
 
-print("Hello " + name)
+print("Hello " + name + ". Your name has " + str(len(name)) + " characters, right?")
 
 name = "Jack"
 
-print("Hello " + name)
+print("Hello " + name + ". Your name has " + str(len(name)) + " characters, right?")
+
+print(len(input("What is your name again?")))
---
+++ Day 1/task/task4a.py
@@ -0,0 +1,11 @@
+"""
+This module contains activities referencing 100 Days of Python course.
+"""
+
+# Python Variables
+# print(len(input("What is your name again?")))
+
+username = input("Hello, what is your name?")
+length = len(username)
+
+print("Your username should be " + str(length) + " characters long.")
