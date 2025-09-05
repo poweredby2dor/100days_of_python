316ace8a1e0b406fa527dc5a711d535263f35536
2dor <poweredby2dor@gmail.com>
2025-09-05 22:48:11 +0300

    Day 1 commit

---
+++ Day 1/task/task4.py
@@ -0,0 +1,13 @@
+"""
+This module contains activities referencing 100 Days of Python course.
+"""
+# pylint: disable=invalid-name
+
+# Python Variables
+name = input("What is your name Sir?")
+
+print("Hello " + name)
+
+name = "Jack"
+
+print("Hello " + name)
