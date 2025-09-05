eb047db381ab9e58e85ccc0fd54c3299e595d4ee
2dor <poweredby2dor@gmail.com>
2025-09-05 22:24:31 +0300

    Day 1 commit

---
+++ Day 1/task/task2.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+
+"""
+This module contains activities referencing 100 Days of Python course.
+"""
+
+print("Hello World!\n Hello World!\n  Hello World!\n")
+
+print("Hello" + " " + "Tudor!")
