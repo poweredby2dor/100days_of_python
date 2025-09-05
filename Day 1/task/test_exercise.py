7c1c7ee8de6891d3c191df249c57e6242f231408
2dor <poweredby2dor@gmail.com>
2025-09-05 23:20:57 +0300

    Day 1 commit

--- Day 1/task/test_exercise.py
+++ Day 1/task/test_exercise.py
@@ -8,4 +8,4 @@ print("Welcome to the Band Name Generator.")
 print("Your band name could be " + input("What's the name of the city you grew up in? ") + " " + input("What is your pet's name? "))
 
 # Variables could also be used for this exercise
-# TO DO
+# soft-to-do
