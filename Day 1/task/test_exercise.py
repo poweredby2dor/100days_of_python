44112e96270c11167f038abe106b15821f083496
2dor <poweredby2dor@gmail.com>
2025-09-05 23:19:38 +0300

    Day 1 commit

---
+++ Day 1/task/test_exercise.py
@@ -0,0 +1,11 @@
+"""
+This module contains activities referencing 100 Days of Python course.
+"""
+# Test exercise
+
+print("Welcome to the Band Name Generator.")
+
+print("Your band name could be " + input("What's the name of the city you grew up in? ") + " " + input("What is your pet's name? "))
+
+# Variables could also be used for this exercise
+# TO DO
