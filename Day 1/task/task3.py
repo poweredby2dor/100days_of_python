9308874c6856df27f1f2c318dc0ff5d09aa43d80
2dor <poweredby2dor@gmail.com>
2025-09-05 22:42:31 +0300

    Day 1 commit

--- Day 1/task/task3.py
+++ Day 1/task/task3.py
@@ -4,4 +4,6 @@
 This module contains activities referencing 100 Days of Python course.
 """
 
+# This line of code shows an example of printing, input and concatenation.
+# Also, it puts an emphasis on order of interpretation of Python.
 print("\nHello " + input("What is your name?") + "!")
