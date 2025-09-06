e73921b757576a186a136e64ddd93c7030750d62
2dor <poweredby2dor@gmail.com>
2025-09-06 21:32:00 +0300

    Day 2 commit

--- Day 2/Tip Calculator Project/task5.py
+++ Day 2/Tip Calculator Project/task5.py
@@ -4,7 +4,7 @@ This module contains an exercise for 100 Days of Python
 
 # Tip Calculator
 
-print("Welcome to the tip calculator!\n")
+print("Welcome to the tip calculator!")
 total = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12 or 15 percent? "))
 people = int(input("How many people will split this bill? "))
