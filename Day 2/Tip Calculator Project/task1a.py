e3bf6ec6cd13193d3dcd715f6ba26e9317bc4b0a
2dor <poweredby2dor@gmail.com>
2025-09-06 20:17:34 +0300

    Day 2 commit

--- Day 2/Tip Calculator Project/task1a.py
+++ Day 2/Tip Calculator Project/task1a.py
@@ -7,3 +7,16 @@ This module contains an exercise for 100 Days of Python
 
 print("Hello"[4])
 print("Hello"[-1])
+
+# Integer, whole number
+print(123 + 345)
+
+# Large integers
+print(1_23_456_789)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
---
+++ Day 2/Tip Calculator Project/task1b.py
@@ -0,0 +1,22 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+# Python primitive data types
+# Strings, Integers, Floats, Booleans
+
+print("Hello"[4])
+print("Hello"[-1])
+
+# Integer, whole number
+print(123 + 345)
+
+# Large integers
+print(1_23_456_789)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
