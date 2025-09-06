cfeb6a8e232b2cc95c9ebec5611d901351d6c8b6
2dor <poweredby2dor@gmail.com>
2025-09-06 20:55:18 +0300

    Day 2 commit

---
+++ Day 2/Tip Calculator Project/task3.py
@@ -0,0 +1,18 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+# TypeError, Type Checking and Type Conversion
+# Mathematical Operations
+
+print(6 // 2)
+print(7 // 2)
+
+print(2 ** 2)
+
+# PEMDAS
+# Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
---
+++ Day 2/Tip Calculator Project/task4.py
@@ -0,0 +1,20 @@
+"""
+This module contains an exercise for 100 Days of Python
+
+The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight.
+This is the formula used to calculate it:
+bmi is equal to the person's weight divided by the person's height squared.
+Convert this sentence into code on line 6.
+
+"""
+
+# BMI Calculator
+
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = round((weight / (height ** 2)), 1)
+
+print(bmi)
