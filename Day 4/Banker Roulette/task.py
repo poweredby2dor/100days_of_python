37669eef5d2d9a266620ed639996d5e344ea5658
2dor <poweredby2dor@gmail.com>
2025-09-09 20:57:21 +0300

    Day 4b commit

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,12 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# Hardcoded
+print(friends[random.randint(0, 4)])
+
+# Style points
+print(random.choice(friends))
