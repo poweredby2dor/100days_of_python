78aa476f48618a9b1bbe8d22d8e877726ce38065
2dor <poweredby2dor@gmail.com>
2025-09-12 21:41:27 +0300

    Day 5 commit

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,11 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+# Do something to each item in a list
+
 fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    #print(fruit)
+    print(fruit + " pie")
