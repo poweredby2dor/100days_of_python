49fe9a49bd761f29c081e6a0910c0cd43313498e
2dor <poweredby2dor@gmail.com>
2025-09-15 20:51:35 +0300

    Day 6b commit

--- Day 6/Functions/while_loops.py
+++ Day 6/Functions/while_loops.py
@@ -2,9 +2,11 @@
 This module contains an exercise for 100 Days of Python
 """
 
-number_of_hurdles = 6
+number_of_hurdles = 3
 
 while number_of_hurdles > 0:
     print("Jump!")
     number_of_hurdles -= 1
     print(number_of_hurdles)
+
+print("\nThe While loop has ended")
