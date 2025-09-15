722a707415dc1afd1fd754c25088e966adf5113c
2dor <poweredby2dor@gmail.com>
2025-09-15 22:00:16 +0300

    Day 7 commit

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,8 +1,20 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+word = random.choice(word_list)
+length = len(word)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Welcome to Hangman. Choose a letter: ")
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+# Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it is, "Wrong" if it's not.
+for letter in word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
