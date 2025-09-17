475057db5ea90a3077454cc355c0e8cdfc784e6d
2dor <poweredby2dor@gmail.com>
2025-09-17 23:33:44 +0300

    Day 7b commit

--- Day 7/Step 3/main.py
+++ Day 7/Step 3/main.py
@@ -8,21 +8,37 @@ word_list = ["aardvark", "baboon", "camel"]
 # Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 word = list(random.choice(word_list))
 length = len(word)
+attempts = length
 
 placeholder = []
+used_chars = []
 for i in range(0, length):
     placeholder.append("_")
 
+# to be removed
 print(''.join(placeholder))
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("\nWelcome to Hangman. Choose a letter: ").lower()
+print("Welcome to a game of Hangman!")
 
-# Create a display where the good letter is shown in the word
+# Use a while loop to let the user guess again
+while attempts > 0:
+    guess = input("\nChoose a letter: ").lower()
 
-for i in range(0, length):
-    if guess == word[i]:
-        placeholder[i] = word[i]
+    for i in range(0, length):
+        if guess == word[i]:
+            placeholder[i] = word[i]
 
-placeholder = ''.join(placeholder)
-print(f"\n{placeholder}")
+    if guess in used_chars:
+        print("Hey, you already told me that letter!")
+    elif guess in placeholder:
+        print("You got one!\n\n\n\n\n")
+        print(''.join(placeholder))
+    else:
+        attempts -= 1
+        print("Oops, yry again!\n\n\n\n\n")
+        print(''.join(placeholder))
+
+    used_chars.append(guess)
+
+else:
+    print("You've been HUNG!")
