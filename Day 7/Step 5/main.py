1baead3a4a41f34706b6a97c74e640340bfd50c4
2dor <poweredby2dor@gmail.com>
2025-09-29 21:53:13 +0300

    Day 7c commit

--- Day 7/Step 5/main.py
+++ Day 7/Step 5/main.py
@@ -2,70 +2,12 @@
 This module contains an exercise for 100 Days of Python
 """
 import random
+import hangman_words
+import hangman_art
 
-# 1 - use words from list
-# 2 - use stages from file
-# 3 - import the logo from art file
-# 4 - verify already chosen letter
-# 5 - let the player know he lost a life
 # 6 - let the player know how many lives left
 
-stages = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
- / \\  |
-      |
-=========''']
-
-
-word_list = ["aardvark", "baboon", "camel"]
-
-# Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-word = list(random.choice(word_list))
+word = list(random.choice(hangman_words.word_list))
 length = len(word)
 attempts = 6
 game_over = False
@@ -75,10 +17,9 @@ used_chars = []
 for i in range(0, length):
     placeholder.append("_")
 
-print("Welcome to a game of Hangman!\nThis is your word:")
+print(f"{hangman_art.logo3}\nThis is your word:")
 print(''.join(placeholder))
 
-# Use a while loop to let the user guess again
 while not game_over:
     guess = input("\nChoose a letter: ").lower()
 
@@ -91,21 +32,21 @@ while not game_over:
     elif guess in placeholder:
         if "_" not in placeholder:
             game_over = True
-            print("\nYou won! Great job!")
+            print(f"\nYou won!\n{hangman_art.logo2}")
         else:
             print("\nYou got one!\n")
-            print(stages[6 - attempts])
+            print(hangman_art.stages[attempts])
             print(''.join(placeholder))
     else:
         attempts -= 1
-        print(stages[6 - attempts])
+        print(hangman_art.stages[attempts])
         print(''.join(placeholder))
         if attempts == 0:
             game_over = True
             print("\nYou've been HUNG!")
         else:
-            print("Oops, try again!\n")
-            print(stages[6 - attempts])
-            print(''.join(placeholder))
+            print(f"Oops, try again! You've lost a life!\n Lives left: {attempts}")
+            # print(hangman_art.stages[attempts])
+            # print(''.join(placeholder))
 
     used_chars.append(guess)
