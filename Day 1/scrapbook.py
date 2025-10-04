e6699b7c1cb06a948d8cf7995efdcfc7cf60077b
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2025-10-04 13:58:46 +0300

    Day 9 commit
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

---
+++ Day 1/scrapbook.py
@@ -0,0 +1,9 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+# Used these to assign the correct account when pushing to git repo
+"""
+git config --global user.name "Tudor Cernat"
+git config --global user.email "36172873+poweredby2dor@users.noreply.github.com"    
+"""
