a0547994732a68d9c93a5456f9df5b8e77c5c0b3
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2025-10-05 22:26:48 +0300

    Day 10 commit
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -5,7 +5,6 @@ from art import logo
 
 print(logo)
 loop = True
-value1 = int(input("Provide a first number:\n "))
 
 
 def add(n1, n2):
@@ -31,12 +30,17 @@ operations = {
     "/": divide,
 }
 
-while loop:
-    operand = input("What operation do you want to execute? Type a symbol + - * /\n")
-    while operand not in operations.keys():
+
+def calculator():
+    value1 = int(input("\n\nProvide a first number:\n "))
+    while loop:
         operand = input("What operation do you want to execute? Type a symbol + - * /\n")
-    value2 = int(input("Provide a second number:\n "))
-    value1 = operations[operand](value1, value2)
-    if input(f"Result is {value1}. Do you want to make another calculation? Type y or n:\n").lower() == "n":
-        loop = False
-        print("Thank you.")
+        while operand not in operations.keys():
+            operand = input("What operation do you want to execute? Type a symbol + - * /\n")
+        value2 = int(input("Provide a second number:\n "))
+        value1 = operations[operand](value1, value2)
+        if input(f"Result is {value1}. Do you want to make another calculation with it ? Type y or n:\n").lower() == "n":
+            calculator()
+
+
+calculator()
