5a4d44be1ed6a32a2504bee6e564de1a076d7a0b
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2025-10-08 23:21:34 +0300

    Day 10b commit
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -14,14 +14,17 @@ player_wants_to_play = False
 def start_game(first_game):
     global player_wants_to_play
     if first_game:
-        play = input("Do you want to play a game of Blackjack? Type 'y' or 'n'\n")
+        play = input("\n\nDo you want to play a game of Blackjack? Type 'y' or 'n'\n")
     else:
-        play = input("Do you want to play another game of Blackjack? Type 'y' or 'n'\n")
+        play = input("\n\nDo you want to play another game of Blackjack? Type 'y' or 'n'\n")
     if play == "y":
         player_wants_to_play = True
         for i in range(0, 2):
             player.append(random.choice(cards))
             dealer.append(random.choice(cards))
+
+        # Natural Blackjack
+        check_cards(True)
 
         print(f"{logo}\nThank you for playing, let's go.\n\n Dealer has [{dealer[0]}] and [X]\n  You have [{player[0]}] and [{player[1]}].")
     elif play == "n":
@@ -33,19 +36,22 @@ def start_game(first_game):
 
 
 def run_game():
+    check_cards(False)
+
     play = input(f"   Your score {sum(player)}, type 'y' to get another card, type 'n' to pass:\n")
     if play == "y":
         player.append(random.choice(cards))
         if sum(player) > 21:
-            print("Player loses, house wins.")
-            restart_game()
-        elif sum(player) == 21 and sum(dealer) == 21:
-            print("Draw!")
-            restart_game()
-        elif sum(player) == 21 and sum(dealer) != 21:
-            # Evaluate if the dealer can draw safely?
+            print(f"{sum(player)} points, player loses, house wins.")
+        elif sum(player) == 21 and sum(dealer) < 21:
+            # TODO Evaluate if the dealer can draw safely?
             print("Blackjack! Player wins.")
-            restart_game()
+        elif sum(dealer) == 21:
+            print("House has Blackjack!")
+
+        if sum(player) > 21 or (sum(player) == 21 and sum(dealer) < 21) or sum(dealer) > 21:
+            show_cards(and_restart=True)
+
     elif play == "n":
         evaluate_cards()
     else:
@@ -63,13 +69,40 @@ def restart_game():
 
 def evaluate_cards():
     if sum(player) > sum(dealer):
-        print(f"Players wins!\n With {sum(player)} points versus {sum(dealer)} points on the house.\n\n\n")
+        show_cards(False)
+        print(f"Player wins!\n With {sum(player)} points versus {sum(dealer)} points on the house.\n\n\n")
     elif sum(player) == sum(dealer):
+        show_cards(False)
         print(f"It's a draw.\n\n\n")
     else:
+        show_cards(False)
         print(f"House wins with {sum(dealer)} points. Better luck next time.\n\n\n")
 
     restart_game()
+
+
+def check_cards(natural_blackjack):
+    if sum(player) == 21 and sum(dealer) == 21:
+        print("Push. It's a tie.")
+    elif sum(player) == 21:
+        if natural_blackjack:
+            print("Natural Blackjack!")
+        else:
+            print("Blackjack! you are lucky!")
+    elif sum(dealer) == 21:
+        if natural_blackjack:
+            print("House wins with Natural Blackjack")
+        else:
+            print("House wins.")
+
+    if sum(player) == 21 or sum(dealer) == 21:
+        show_cards(and_restart=True)
+
+
+def show_cards(and_restart):
+    print(f"House had {dealer} and you had {player}\n")
+    if and_restart:
+        restart_game()
 
 
 # Code runs here
