b1e149a1c2db7a1dd62038e3797d74b82d9d2ac8
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2025-10-19 18:13:21 +0300

    Day 12 commit
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,3 +1,8 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+
 enemies = 1
 
 
@@ -8,3 +13,18 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+
+# Global Scope
+player_health = 10
+
+
+# Local Scope
+def drink_potion():
+    potion_strength = 2
+    global player_health
+    player_health = player_health + potion_strength
+    print(player_health)
+
+
+drink_potion()
