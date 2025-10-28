5f8f66586a614243bd4c458fb5b61a5c6a8f8361
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2025-10-28 19:58:38 +0200

    Commit for Day 16
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

--- Day 16/coffee_machine/main.py
+++ Day 16/coffee_machine/main.py
@@ -6,6 +6,21 @@ This module contains an exercise for 100 Days of Python
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+machine = CoffeeMaker()
+menu = Menu()
+cash_machine = MoneyMachine()
+
+
+def refill_machine(machine_name):
+    """
+    Advanced Python - refill
+    :param machine_name:
+    :return:
+    """
+    print("Refilling machine...")
+    for item in machine_name.resources:
+        # Double them
+        machine_name.resources[item] += machine_name.resources[item]
 
 
 def use_machine():
@@ -13,25 +28,37 @@ def use_machine():
     Main function for Coffee Maker
     :return:
     """
-    machine = CoffeeMaker()
-    menu = Menu()
-    cash_machine = MoneyMachine()
+    run_machine = True
+    drink = None
 
-    choice = input("\nWhat would you like? (espresso/latte/cappuccino):\n").lower()
-    drink = menu.find_drink(choice)
+    while run_machine:
+        options = menu.get_items()
+        choice = input(f"\nWhat would you like? /{options}:\n").lower()
+        if choice == "off":
+            run_machine = False
+        elif choice == "report":
+            machine.report()
+            cash_machine.report()
+        else:
+            drink = menu.find_drink(choice)
 
-    if choice == "report":
-        machine.report()
-        cash_machine.report()
-        use_machine()
-    elif drink:
-        # Check resources
-        if machine.is_resource_sufficient(drink):
-            # Check money
-            cash_machine.make_payment(drink.cost)
-        # Do coffee
-        print(choice)
+        if drink:
+            # Check resources
+            if machine.is_resource_sufficient(drink):
+                # Check money
+                if cash_machine.make_payment(drink.cost):
+                    # Do coffee
+                    machine.make_coffee(drink)
+                else:
+                    if cash_machine.make_payment(drink.cost):
+                        # Do coffee again
+                        machine.make_coffee(drink)
+            else:
+                run_machine = False
 
+
+# Advanced Python exercise
+refill_machine(machine)
 
 # Here runs the code
 use_machine()
