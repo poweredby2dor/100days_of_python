c616957dff50d9704c0db4ed92ba86f7caab9c38
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2026-04-29 00:22:44 +0300

    Commit for Day 17
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

---
+++ Day 17/main.py
@@ -0,0 +1,7 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
+
+# Create a Question class with a __init()__ method with 2 attributes: text and answer attribute.\
+
+#
---
+++ Day 17/quiz_brain.py
@@ -0,0 +1,3 @@
+"""
+This module contains an exercise for 100 Days of Python
+"""
