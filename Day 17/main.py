cc9812b7aa59653a450b3eb7df664298cc88f321
Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>
2026-04-29 08:26:52 +0300

    Commit for Day 17
    Signed-off-by: Tudor Cernat <36172873+poweredby2dor@users.noreply.github.com>

--- Day 17/main.py
+++ Day 17/main.py
@@ -2,6 +2,16 @@
 This module contains an exercise for 100 Days of Python
 """
 
-# Create a Question class with a __init()__ method with 2 attributes: text and answer attribute.\
+# Create a Question class with a __init()__ method with 2 attributes: text and answer attribute.
 
-#
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.question = q_text
+        self.answer = q_answer
+        print("Question class has been created.")
+
+
+new_q = Question("text", "answer")
+
+print(new_q.question)
